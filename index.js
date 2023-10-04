978e912490086b42d5969fa2638257d8ff419edc
Petermwansa <mwansapeter800@gmail.com>
2023-10-04 13:06:12 +0300

    add the files

---
+++ index.js
@@ -0,0 +1,18 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 300;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add("show-animate");
+        } 
+        
+        // if you want to use repeating animation on scroll 
+        else {
+            sec.classList.remove('show-animate')
+        }
+    })
+}
